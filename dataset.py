871533ab0b8e9b90fb8d958458fe0d37be70337e
AlphaTheConquerer <96761127+AlphaTheConquerer@users.noreply.github.com>
2024-03-30 16:37:04 +0530

    Add files via upload

---
+++ dataset.py
@@ -0,0 +1,296 @@
+import os
+import sys
+import pickle
+
+import io
+import matplotlib.pyplot as plt
+import numpy as np
+import torch
+from torch.utils.data import Dataset
+import os
+import torch
+import torch.nn as nn
+from torch.utils.data import Dataset
+
+import tensorflow_datasets as tfds
+
+class MalimgDataset(Dataset):
+    """Custom dataset class for the Malimg dataset."""
+    def __init__(self, data_dir, transform=None, target_transform=None):
+        """
+        Args:
+            data_dir (str): The path to the data directory containing the Malimg dataset.
+            transform (callable, optional): A callable to apply to the input image.
+            target_transform (callable, optional): A callable to apply to the target labels.
+        """
+        self.data_dir = data_dir
+        self.dataset = tfds.load('malimg', data_dir=data_dir, download=False, as_supervised=True,
+                                  split=['train'], with_info=False, shuffle_files=True,
+                                  transfer_values=False)
+        self.transform = transform
+        self.target_transform = target_transform
+
+    def __len__(self):
+        """Return the length of the dataset."""
+        return len(self.dataset)
+
+    def __getitem__(self, idx):
+        """Gets the input tensor and target label given the index."""
+        x, y = self.dataset[idx]
+        x = np.expand_dims(x, -1)
+        if self.transform:
+            x = self.transform(x)
+
+        if self.target_transform:
+            y = self.target_transform(y)
+
+        return x, y
+
+def malimg_preprocess(tensor):
+    """Preprocessing function for the Malimg dataset."""
+    # Normalize the pixel values to [0, 1]
+    tensor = tensor.astype('float32') / 255
+
+    # Add a 4th color channel with a constant value of 1.0
+    tensor = np.expand_dims(tensor, -1)
+
+    # Convert the NumPy array to a PyTorch tensor
+    tensor = torch.from_numpy(tensor.transpose(2, 0, 1))
+
+    return tensor
+
+# Modify the InceptionV3 class to accept input channels equal to the number of channels in the Malimg dataset (1)
+class Inception(nn.Module):
+    def __init__(self, input_channels, n1x1, n3x3_reduce, n3x3, n5x5_reduce, n5x5, pool_proj):
+        super().__init__()
+
+        #1x1conv branch
+        self.b1 = nn.Sequential(
+            nn.Conv2d(input_channels, n1x1, kernel_size=1),
+            nn.BatchNorm2d(n1x1),
+            nn.ReLU(inplace=True)
+        )
+
+        #1x1conv -> 3x3conv branch
+        self.b2 = nn.Sequential(
+            nn.Conv2d(input_channels, n3x3_reduce, kernel_size=1),
+            nn.BatchNorm2d(n3x3_reduce),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(n3x3_reduce, n3x3, kernel_size=3, padding=1),
+            nn.BatchNorm2d(n3x3),
+            nn.ReLU(inplace=True)
+        )
+
+        #1x1conv -> 5x5conv branch
+        #we use 2 3x3 conv filters stacked instead
+        #of 1 5x5 filters to obtain the same receptive
+        #field with fewer parameters
+        self.b3 = nn.Sequential(
+            nn.Conv2d(input_channels, n5x5_reduce, kernel_size=1),
+            nn.BatchNorm2d(n5x5_reduce),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(n5x5_reduce, n5x5, kernel_size=3, padding=1),
+            nn.BatchNorm2d(n5x5, n5x5),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(n5x5, n5x5, kernel_size=3, padding=1),
+            nn.BatchNorm2d(n5x5),
+            nn.ReLU(inplace=True)
+        )
+
+        #3x3pooling -> 1x1conv
+        #same conv
+        self.b4 = nn.Sequential(
+            nn.MaxPool2d(3, stride=1, padding=1),
+            nn.Conv2d(input_channels, pool_proj, kernel_size=1),
+            nn.BatchNorm2d(pool_proj),
+            nn.ReLU(inplace=True)
+        )
+
+    def forward(self, x):
+        return torch.cat([self.b1(x), self.b2(x), self.b3(x), self.b4(x)], dim=1)
+
+
+class GoogleNet(nn.Module):
+
+    def __init__(self, num_class=100):
+        super().__init__()
+        self.prelayer = nn.Sequential(
+            nn.Conv2d(3, 64, kernel_size=3, padding=1, bias=False),
+            nn.BatchNorm2d(64),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(64, 64, kernel_size=3, padding=1, bias=False),
+            nn.BatchNorm2d(64),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(64, 192, kernel_size=3, padding=1, bias=False),
+            nn.BatchNorm2d(192),
+            nn.ReLU(inplace=True),
+        )
+
+        #although we only use 1 conv layer as prelayer,
+        #we still use name a3, b3.......
+        self.a3 = Inception(192, 64, 96, 128, 16, 32, 32)
+        self.b3 = Inception(256, 128, 128, 192, 32, 96, 64)
+
+        ##"""In general, an Inception network is a network consisting of
+        ##modules of the above type stacked upon each other, with occasional
+        ##max-pooling layers with stride 2 to halve the resolution of the
+        ##grid"""
+        self.maxpool = nn.MaxPool2d(3, stride=2, padding=1)
+
+        self.a4 = Inception(480, 192, 96, 208, 16, 48, 64)
+        self.b4 = Inception(512, 160, 112, 224, 24, 64, 64)
+        self.c4 = Inception(512, 128, 128, 256, 24, 64, 64)
+        self.d4 = Inception(512, 112, 144, 288, 32, 64, 64)
+        self.e4 = Inception(528, 256, 160, 320, 32, 128, 128)
+
+        self.a5 = Inception(832, 256, 160, 320, 32, 128, 128)
+        self.b5 = Inception(832, 384, 192, 384, 48, 128, 128)
+
+        #input feature size: 8*8*1024
+        self.avgpool = nn.AdaptiveAvgPool2d((1, 1))
+        self.dropout = nn.Dropout2d(p=0.4)
+        self.linear = nn.Linear(1024, num_class)
+
+    def forward(self, x):
+        x = self.prelayer(x)
+        x = self.maxpool(x)
+        x = self.a3(x)
+        x = self.b3(x)
+
+        x = self.maxpool(x)
+
+        x = self.a4(x)
+        x = self.b4(x)
+        x = self.c4(x)
+        x = self.d4(x)
+        x = self.e4(x)
+
+        x = self.maxpool(x)
+
+        x = self.a5(x)
+        x = self.b5(x)
+
+        #"""It was found that a move from fully connected layers to
+        #average pooling improved the top-1 accuracy by about 0.6%,
+        #however the use of dropout remained essential even after
+        #removing the fully connected layers."""
+        x = self.avgpool(x)
+        x = self.dropout(x)
+        x = x.view(x.size()[0], -1)
+        x = self.linear(x)
+
+        return x
+
+def googlenet():
+    return GoogleNet()
+
+class Bottleneck(nn.Module):
+    def __init__(self, in_channels, growth_rate):
+        super().__init__()
+        #"""In  our experiments, we let each 1×1 convolution
+        #produce 4k feature-maps."""
+        inner_channel = 4 * growth_rate
+
+        #"""We find this design especially effective for DenseNet and
+        #we refer to our network with such a bottleneck layer, i.e.,
+        #to the BN-ReLU-Conv(1×1)-BN-ReLU-Conv(3×3) version of H ` ,
+        #as DenseNet-B."""
+        self.bottle_neck = nn.Sequential(
+            nn.BatchNorm2d(in_channels),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(in_channels, inner_channel, kernel_size=1, bias=False),
+            nn.BatchNorm2d(inner_channel),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(inner_channel, growth_rate, kernel_size=3, padding=1, bias=False)
+        )
+
+    def forward(self, x):
+        return torch.cat([x, self.bottle_neck(x)], 1)
+
+#"""We refer to layers between blocks as transition
+#layers, which do convolution and pooling."""
+class Transition(nn.Module):
+    def __init__(self, in_channels, out_channels):
+        super().__init__()
+        #"""The transition layers used in our experiments
+        #consist of a batch normalization layer and an 1×1
+        #convolutional layer followed by a 2×2 average pooling
+        #layer""".
+        self.down_sample = nn.Sequential(
+            nn.BatchNorm2d(in_channels),
+            nn.Conv2d(in_channels, out_channels, 1, bias=False),
+            nn.AvgPool2d(2, stride=2)
+        )
+
+    def forward(self, x):
+        return self.down_sample(x)
+
+#DesneNet-BC
+#B stands for bottleneck layer(BN-RELU-CONV(1x1)-BN-RELU-CONV(3x3))
+#C stands for compression factor(0<=theta<=1)
+class DenseNet(nn.Module):
+    def __init__(self, block, nblocks, growth_rate=12, reduction=0.5, num_class=100):
+        super().__init__()
+        self.growth_rate = growth_rate
+
+        #"""Before entering the first dense block, a convolution
+        #with 16 (or twice the growth rate for DenseNet-BC)
+        #output channels is performed on the input images."""
+        inner_channels = 2 * growth_rate
+
+        #For convolutional layers with kernel size 3×3, each
+        #side of the inputs is zero-padded by one pixel to keep
+        #the feature-map size fixed.
+        self.conv1 = nn.Conv2d(3, inner_channels, kernel_size=3, padding=1, bias=False)
+
+        self.features = nn.Sequential()
+
+        for index in range(len(nblocks) - 1):
+            self.features.add_module("dense_block_layer_{}".format(index), self._make_dense_layers(block, inner_channels, nblocks[index]))
+            inner_channels += growth_rate * nblocks[index]
+
+            #"""If a dense block contains m feature-maps, we let the
+            #following transition layer generate θm output feature-
+            #maps, where 0 < θ ≤ 1 is referred to as the compression
+            #fac-tor.
+            out_channels = int(reduction * inner_channels) # int() will automatic floor the value
+            self.features.add_module("transition_layer_{}".format(index), Transition(inner_channels, out_channels))
+            inner_channels = out_channels
+
+        self.features.add_module("dense_block{}".format(len(nblocks) - 1), self._make_dense_layers(block, inner_channels, nblocks[len(nblocks)-1]))
+        inner_channels += growth_rate * nblocks[len(nblocks) - 1]
+        self.features.add_module('bn', nn.BatchNorm2d(inner_channels))
+        self.features.add_module('relu', nn.ReLU(inplace=True))
+
+        self.avgpool = nn.AdaptiveAvgPool2d((1, 1))
+
+        self.linear = nn.Linear(inner_channels, num_class)
+
+    def forward(self, x):
+        output = self.conv1(x)
+        output = self.features(output)
+        output = self.avgpool(output)
+        output = output.view(output.size()[0], -1)
+        output = self.linear(output)
+        return output
+
+    def _make_dense_layers(self, block, in_channels, nblocks):
+        dense_block = nn.Sequential()
+        for index in range(nblocks):
+            dense_block.add_module('bottle_neck_layer_{}'.format(index), block(in_channels, self.growth_rate))
+            in_channels += self.growth_rate
+        return dense_block
+
+def densenet121():
+    return DenseNet(Bottleneck, [6,12,24,16], growth_rate=32)
+
+def densenet169():
+    return DenseNet(Bottleneck, [6,12,32,32], growth_rate=32)
+
+def densenet201():
+    return DenseNet(Bottleneck, [6,12,48,32], growth_rate=32)
+
+def densenet161():
+    return DenseNet(Bottleneck, [6,12,36,24], growth_rate=48)
+
+
